6df3f3bbaab564a0d641e7de29dee6b39fa0ffce
VivanPanda <vivanpanda13@gmail.com>
2025-07-27 19:07:43 +0400

    Add photoresistor code

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+int lightPin = A7;
+int lightVal;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(lightPin, INPUT);
+  // pinMode(redPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lightVal = analogRead(lightPin);
+  Serial.println(lightVal);
 }
