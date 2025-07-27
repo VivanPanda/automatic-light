01ce2ff79d53391ab988ebbe29b3cdb3521e8115
VivanPanda <vivanpanda13@gmail.com>
2025-07-27 19:19:22 +0400

    Complete program

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,21 @@
 int lightPin = A7;
 int lightVal;
+int redPin = 8;
 
 void setup() {
   Serial.begin(9600);
   pinMode(lightPin, INPUT);
-  // pinMode(redPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
 }
 
 void loop() {
   lightVal = analogRead(lightPin);
   Serial.println(lightVal);
+  delay(500);
+  if (lightVal >= 800) {
+    digitalWrite(redPin, LOW); // turn off lights if above 800
+  }
+  if (lightVal < 600) {
+    digitalWrite(redPin, HIGH); // turn on lights if below 600
+  }
 }
